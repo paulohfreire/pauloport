56fcb6a2c9b90f350097564e3f6374a30b18ff9c
Paulohfreire <paulo.freire53@gmail.com>
2020-12-14 18:37:34 -0300

    initial commit

---
+++ config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteTitle: 'Paulo Portfólio', // <title>
+  manifestName: 'Resume',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/start-resume/`, // This path is subpath of your hosting https://domain/portfolio
+  firstName: 'Paulo',
+  lastName: 'Henrique Freire',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/paulohfreire',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://linkedin.com/in/phfreire/',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/freireopaulo',
+    },
+  ],
+  email: 'paulo.freire53@gmail.com',
+  phone: '+55-84-999191072',
+  address: 'Natal, BR',
+};
